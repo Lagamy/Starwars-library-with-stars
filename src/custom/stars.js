cd7135e77ea5abe72cacec658c15f003fb36c9a1
LagAmy <ilyatima31@gmail.com>
2024-12-04 00:40:20 +0200

    Culling methods implemented(needs further optimisation)

--- src/custom/stars.js
+++ src/custom/stars.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const Stars = ({ Loading, Class, Size }) => {
+const Stars = ({ Loading, Class, Size, paralaxSpeed }) => {
   let starsContainer = useRef(null); // Reference to the stars container
-  const [stars, setStars] = useState([]);  // Store star positions and colors
+  let stars = null;  // Store star positions and colors
+  let visibleStars;
   let lowestStarLocation = 0 // due to infinite scroll - I need to generate new set of stars downwards from lowest star, avoiding double "staring" area. 
-
+  
   useEffect(() => {
     function handleResize() {
       starsContainer.current.style.boxShadow = "";
@@ -14,14 +15,10 @@ const Stars = ({ Loading, Class, Size }) => {
 
     function handleParalax()
     {
-      const foreground = document.querySelector('.foreground-stars');
-      //console.log(foreground)
-      foreground.style.setProperty('--translateY', `${window.scrollY * 0.6}px`);
-      //console.log("ScrollY: ", window.scrollY);
-      //console.log("Foreground Transform: ", 'translate3d(0px, ' + window.scrollY * -3 + 'px, 0)');
-      const middleground = document.querySelector('.middleground-stars');
-      //console.log(middleground)
-      middleground.style.setProperty('--translateY', `${window.scrollY * 0.3}px`);
+      let paralax = window.scrollY * paralaxSpeed
+      updateVisibleStars(paralax);
+    
+      starsContainer.current.style.setProperty('--translateY', `${paralax}px`);
     }
     window.addEventListener('scroll', handleParalax)
     window.addEventListener('resize', handleResize)
@@ -45,6 +42,10 @@ const Stars = ({ Loading, Class, Size }) => {
 
 
 
+  useEffect(() => {
+
+  })
+
   // const generateStars = (pageWidth) => {
   //   if(!Loading) {
   //     // clear any existing stars
@@ -66,6 +67,7 @@ const Stars = ({ Loading, Class, Size }) => {
   //     starsContainer.current.style.boxShadow = boxShadowArray.join(", ");
   //   }
   // };
+
   const generateStars = (pageWidth) => {
   if(!Loading) {
     // clear any existing stars
@@ -74,27 +76,97 @@ const Stars = ({ Loading, Class, Size }) => {
     }
     const numberStars = 2000;
     const availableHeight = document.documentElement.scrollHeight - lowestStarLocation;
+    let id = 1;
+    const newStars = [];
     for (let i = 0; i < numberStars; i++) {
-      var color = colorPicker();
-      const star = document.createElement("div");
-      //star.classList.add(Class);
-      star.classList.add("star");
-      star.style.top = `${Math.random() * availableHeight}px`;
-      star.style.left = `${Math.random() * pageWidth}px`;
-      star.style.width = `${Size}px`;
-      star.style.backgroundColor = `${color}`;
-      star.style.height = `${Size}px`;
-      star.style.animationDuration = `${Math.random() * 5}s`;
-      star.style.animationDelay = `${Math.random() * 5}s`;
-      star.style.boxShadow = `0px 0px 50px 8px ${color}`
-      star.style.borderRadius = `0px`
-      starsContainer.current.appendChild(star);
-      lowestStarLocation = Math.max(parseFloat(star.style.top), lowestStarLocation);
+      const top = Math.random() * availableHeight;
+      const left = Math.random() * pageWidth;
+      const color = colorPicker();
+      newStars.push({ top, left, color, id });
+      id++;
     }
+    stars = newStars;
+    initializeVisibleStars();
   }
 }
+
+
+
+const initializeVisibleStars = () => {
+    // clear any existing stars
+    const viewportTop = window.scrollY;
+    const viewportBottom = viewportTop + window.innerHeight;
+
+    // Filter for stars in the viewport
+    visibleStars = stars.filter(
+      (star) => star.top >= viewportTop && star.top <= viewportBottom
+    );
+    visibleStars.forEach(({ top, left, color, id }) => {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      star.style.top = `${top}px`;
+      star.style.left = `${left}px`;
+      star.style.width = `${Size}px`;
+      star.style.height = `${Size}px`;
+      star.style.backgroundColor = `${color}`;
+      star.style.boxShadow = `0px 0px 50px 10px ${color}`;
+      star.style.borderRadius = `0px`;
+      star.style.animationDelay = `${Math.random() * 5}s`;
+      star.style.animationDuration = `${Math.random() * 5}s`;
+      star.dataset.id = id;
+      starsContainer.current.appendChild(star);
+    });
+}
+
+const updateVisibleStars = (paralax) => {
+  const viewportTop = window.scrollY;
+  const viewportBottom = viewportTop + window.innerHeight;
+  console.log(paralax)
+  let currentVisibleStars = stars.filter(
+    (star) => star.top + paralax >= viewportTop && star.top + paralax <= viewportBottom// add paralax changes here
+  );
+
+  visibleStars.forEach(({ top, left, color, id }) => {
+    if (!contains(currentVisibleStars, id))
+    {
+      const starElement = document.querySelector(`[data-id="${id}"]`);
+      if (starElement) {
+        //console.log(starElement)
+        starElement.remove();
+      }
+    }
+  });
+
+
+  currentVisibleStars.forEach(({ top, left, color, id }) => {
+    if (!contains(visibleStars, id))
+    {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      star.style.top = `${top}px`;
+      star.style.left = `${left}px`;
+      star.style.width = `${Size}px`;
+      star.style.height = `${Size}px`;
+      star.style.backgroundColor = `${color}`;
+      star.style.boxShadow = `0px 0px 50px 8px ${color}`;
+      star.style.borderRadius = `0px`;
+      star.style.animationDelay = `${Math.random() * 5}s`;
+      star.style.animationDuration = `${Math.random() * 5}s`;
+      star.dataset.id = id; // Store top as data attribute
+      starsContainer.current.appendChild(star);
+    }
+  });
+  visibleStars = currentVisibleStars;
+}
+
   return <div ref={starsContainer} className={`stars  ${Class}`}></div>;
 };
+
+
+const contains = (array, id) => {
+  return array.some((star) => star.id === id)
+}
+
 
 const colorPicker = () => {
     const randNum = Math.floor(Math.random() * 3) + 1; // random integer between 1 and 3
