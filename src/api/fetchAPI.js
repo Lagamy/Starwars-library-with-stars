e581a1f57e8f0a7d006ccb4df7657f44b3231251
LagAmy <ilyatima31@gmail.com>
2025-02-01 19:15:53 +0200

    Simplified fetch due to Api changes

--- src/api/fetchAPI.js
+++ src/api/fetchAPI.js
@@ -2,15 +2,8 @@ import axios from "axios";
 
 export const fetchAPI = async (url, setData, setError) => {
   try {
-    let allData = []; // Array to accumulate all results
-    let nextUrl = url; // Start with the initial URL
-    while (nextUrl) { // Loop until there's no next page
-      const response = await axios.get(nextUrl);
-      
-      allData = [...allData, ...response.data]; // Collect results
-      nextUrl = response.data.next; // Get the next URL if it exists
-    }
-    //console.log(allData);
+    const response = await axios.get(url);
+    const allData = response.data;
     setData(allData); // Set data only once after all fetches are complete
   } catch (err) {
     setError(err.message); // Set error message
